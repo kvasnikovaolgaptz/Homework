c405a39f03f0b3833ffbfd3a602d659dec46fd7a
Olga Kvasnikova <kvasnikovaolgaptz@gmail.com>
2026-03-13 23:07:18 +0300

    черновик

---
+++ src/homework1/Homework1_1.java
@@ -0,0 +1,54 @@
+package homework1;
+
+public class Homework1_1 {
+    public static void main(String[] args) {
+        System.out.println("Квасникова Ольга Николаевна");
+        boolean January = false;
+        boolean MARCH = true;
+
+        char letter = 77;
+        System.out.println(letter);
+
+        int age = 16; // чаще всего будем пользоваться
+        double p = 3.1415926;// чаще всего будем пользоваться
+        age = 17;
+        System.out.println(age);
+
+        int m = 42; // почему не работат?
+        m++;
+
+        System.out.println("Задание 1");
+        int b = 10;
+        int c = 11;
+        int a = 4 * (b+c-1)/2;
+        System.out.println(a);
+
+        System.out.println("Задание 2");
+        int n = 77;
+        int n1 = n/10;
+        int n2 = n%10;
+        System.out.println(n1+n2);
+
+        System.out.println("Задание 3");
+        int d = 789;
+        int d1 = d/100; // 1-ая цифра
+        int d2 = (d/10)%10; // 2-ая цифра
+        int d3 = d%10; // 3-ая цифра
+        System.out.println(d1+d2+d3);
+
+        System.out.println("Задание 4");
+        int d = 789;
+        int d1 = d/100; // 1-ая цифра
+        int d2 = (d/10)%10; // 2-ая цифра
+        int d3 = d%10; // 3-ая цифра
+        System.out.println(d1+d2+d3);
+
+
+
+
+
+
+
+
+    }
+}
