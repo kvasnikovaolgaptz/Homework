3b56a229f43dbbfbd0cd5dd25dad4ab5a68bb57d
Olga Kvasnikova <kvasnikovaolgaptz@gmail.com>
2026-03-14 12:45:43 +0300

    вливание дз1

--- src/homework1/Homework1_1.java
+++ src/homework1/Homework1_1.java
@@ -8,25 +8,25 @@ public class Homework1_1 {
         int b = 10;
         int c = 11;
         int a = 4 * (b+c-1)/2;
-        System.out.println(a);
+        System.out.println("4*(b+c-1)/2="+a);
 
         System.out.println("Задание 2");
         int n = 77;
-        int n1 = n/10;
-        int n2 = n%10;
-        System.out.println(n1+n2);
+        int n1 = n/10; // 1-ая цифра
+        int n2 = n%10; // 2-ая цифра
+        System.out.println(n1+"+"+n2+"="+(n1+n2));
 
         System.out.println("Задание 3");
         int d = 789;
         int d1 = d/100; // 1-ая цифра
         int d2 = (d/10)%10; // 2-ая цифра
         int d3 = d%10; // 3-ая цифра
-        System.out.println(d1+d2+d3);
+        System.out.println(d1+"+"+d2+"+"+d3+"="+(d1+d2+d3));
 
-        System.out.println("Задание 4 перепроверить устала");
-        double h = 77.1;
-        int h_rounder = (int) (h + 0.5);
-        System.out.println(h_rounder);
+        System.out.println("Задание 4");
+        double h = 77.4;
+        int h_rounder = (int) (h+0.5);
+        System.out.println("округление до ближайшего целого "+h_rounder);
 
         System.out.println("Задание 5");
         int q = 789;
@@ -36,10 +36,12 @@ public class Homework1_1 {
         System.out.println("Задание *");
         int f = 1;
         int j = 2;
-        int k = f;
+        int k = j;
         j = f;
         f = k;
 
-        System.out.println(k);
+        System.out.println("Значение f="+f);
+        System.out.println("Значение j="+j);
+
     }
 }
